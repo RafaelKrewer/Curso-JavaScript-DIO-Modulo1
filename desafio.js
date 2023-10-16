fe0eff87d86a5bbcbd476ab0cbd9e37255fc9482
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-16 16:07:35 -0300

    desafio

---
+++ desafio.js
@@ -0,0 +1,10 @@
+var precoCombustivel = 5.79;
+const kmPorLitros = 10;
+const distanciaEmKm = 100;
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+
+var valorTotal = litrosConsumidos * precoCombustivel;
+
+var string = 'R$';
+console.log(string +  valorTotal);
