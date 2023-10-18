cd6b6193ea7b107a4df76e38df2f6b037e9d3887
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-18 18:21:25 -0300

    fim do desafio

--- 002-Condicionais/desafio02.js
+++ 002-Condicionais/desafio02.js
@@ -2,17 +2,15 @@ const precoEtanol = 5.79;
 const precoGasolina = 6.65;
 const kmPorLitros = 10;
 const distanciaEmKm = 100;
-const tipoCombustivel = 'Etanol';
+const tipoCombustivel = 'Gasolina';
+
 
 const litrosConsumidos = distanciaEmKm / kmPorLitros;
 
 if (tipoCombustivel === 'Etanol') {
+    const valorGasto = litrosConsumidos * precoEtanol;
     console.log(valorGasto.toFixed(2));
-
 } else {
+    const valorGasto = litrosConsumidos * precoGasolina;
+    console.log(valorGasto.toFixed(2));
 }
-
-const valorGasto = litrosConsumidos * precoEtanol;
-
-
-
