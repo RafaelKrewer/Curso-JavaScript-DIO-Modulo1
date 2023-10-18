0f1fa18eb0554d0ed7f9033cd5dffab05ca2fcc2
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-18 16:57:55 -0300

    else if

---
+++ 002-Condicionais/aula02.js
@@ -0,0 +1,14 @@
+    const numero = 5;
+
+    const numeroDivisivelPor5 = (numero % 5) === 0;
+
+    if (numeroDivisivelPor5 === 0) {
+        console.log('O número é invalido');
+    } else if (numero === 5) {
+        console.log('O número é invalido');
+    } else if (numeroDivisivelPor5) {
+        console.log('Sim');
+    } else {
+        console.log('Não');
+    }
+
