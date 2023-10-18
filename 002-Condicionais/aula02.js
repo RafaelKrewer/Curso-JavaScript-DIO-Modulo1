70ce2de44dc5c438ee85276e372ad4899b3a7c29
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-18 17:16:03 -0300

    par ou ímpar

--- 002-Condicionais/aula02.js
+++ 002-Condicionais/aula02.js
@@ -1,14 +1,9 @@
     const numero = 5;
+    const ehNumeroPar = (numero % 2) === 0;
 
-    const numeroDivisivelPor5 = (numero % 5) === 0;
-
-    if (numeroDivisivelPor5 === 0) {
-        console.log('O número é invalido');
-    } else if (numero === 5) {
-        console.log('O número é invalido');
-    } else if (numeroDivisivelPor5) {
-        console.log('Sim');
+    if (ehNumeroPar) {
+        console.log('O número é par');
     } else {
-        console.log('Não');
+       console.log('O número é ímpar') 
     }
-
+ 
