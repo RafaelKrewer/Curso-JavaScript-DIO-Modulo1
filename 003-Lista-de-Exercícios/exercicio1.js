c35e4cbc3daa45e893e84e76e50302e997db39a3
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-19 16:10:43 -0300

    rodou ou passou

--- 003-Lista-de-Exercícios/exercicio1.js
+++ 003-Lista-de-Exercícios/exercicio1.js
@@ -4,10 +4,14 @@
 
     const notaMedia = (nota1 + nota2 + nota3 ) / 3;
 
-    console.log(notaMedia);
+    console.log('Sua nota é ' + notaMedia);
 
     if (notaMedia <= 4.9); {
-        console.log('rodou!')
+        console.log('Você rodou, e terá que repetir o ano :(')
     }  
     
-    if (notaMedia => 6.9 ); 
+    if (notaMedia => 6.9 ); {
+        console.log('Parabéns, você passou de ano!')
+    }
+
+    /*if (notaMedia )*/
