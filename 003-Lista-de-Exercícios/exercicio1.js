5db13cc882abfa528e6a2b9ec162928d05bc3f8e
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-19 15:11:45 -0300

    extensões

---
+++ 003-Lista-de-Exercícios/exercicio1.js
@@ -0,0 +1,11 @@
+const nota1 = 5
+const nota2 = 7
+const nota3 = 7
+
+const notaTotal = (nota1 + nota2 + nota3 )/ 3;
+
+console.log(notaTotal);
+
+if (notaTotal <= 4.9); {
+    console.log
+}  
--- 003-Lista-de-Exercícios/exercício1.js
+++
@@ -1,5 +0,0 @@
-const nota1 = 5
-const nota2 = 7
-const nota3 = 7
-
-const notaTotal = nota1 + nota2 + nota3;
