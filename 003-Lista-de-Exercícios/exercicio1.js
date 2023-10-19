1382290f68e426c01054c3e28ad2ce77ef70da10
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-19 16:30:21 -0300

    formatação

--- 003-Lista-de-Exercícios/exercicio1.js
+++ 003-Lista-de-Exercícios/exercicio1.js
@@ -1,17 +1,13 @@
-    const nota1 = 3;
-    const nota2 = 7;
-    const nota3 = 9.5;
+const nota1 = 5;
+const nota2 = 7;
+const nota3 = 9.5;
 
-    const notaMedia = (nota1 + nota2 + nota3 ) / 3;
+const notaMedia = (nota1 + nota2 + nota3) / 3;
 
-    console.log('Sua nota é ' + notaMedia);
+console.log('Sua nota é ' + notaMedia);
 
-    if (notaMedia <= 4.9); {
-        console.log('Você rodou, e terá que repetir o ano :(')
-    }  
-    
-    if (notaMedia => 6.9 ); {
-        console.log('Parabéns, você passou de ano!')
-    }
+if (notaMedia < 5); {
+    console.log('Você rodou, e terá que repetir o ano :(');
 
-    /*if (notaMedia )*/
+}
+
