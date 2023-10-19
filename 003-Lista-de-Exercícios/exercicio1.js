e375c2ef4ce813837440559b71b4aa75b13d969a
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-19 16:52:31 -0300

    else if 2

--- 003-Lista-de-Exercícios/exercicio1.js
+++ 003-Lista-de-Exercícios/exercicio1.js
@@ -1,13 +1,16 @@
-const nota1 = 5;
-const nota2 = 7;
-const nota3 = 9.5;
+const nota1 = 9;
+const nota2 = 8;
+const nota3 = 5;
 
 const notaMedia = (nota1 + nota2 + nota3) / 3;
 
 console.log('Sua nota é ' + notaMedia);
 
-if (notaMedia < 5); {
+if (notaMedia < 5) {
     console.log('Você rodou, e terá que repetir o ano :(');
-
+} else if (notaMedia >= 5 && notaMedia < 7) {
+    console.log('Você ficou em recuperação. Estude mais para passar de ano!');
+} else (notaMedia >= 7); {
+    console.log('Você passou de ano! Aproveite as férias!');
 }
 
