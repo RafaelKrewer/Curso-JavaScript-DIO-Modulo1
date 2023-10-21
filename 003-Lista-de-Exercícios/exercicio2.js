ffd823ad815a15a3b2d8211590af856f49f5ffc6
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-21 14:17:24 -0300

    POW!

--- 003-Lista-de-Exercícios/exercicio2.js
+++ 003-Lista-de-Exercícios/exercicio2.js
@@ -1,4 +1,18 @@
-const peso = 90;
-const altura = 183;
+const peso = 54;
+const altura = 1.67;
+const condicao = peso / (altura * altura);
 
-const imc = peso / altura * altura;
+Math.pow()
+
+if (condicao < 18.5) {
+    console.log('Seu índice corporal IMC é de: ' + condicao.toFixed(0) + '.' + ' Você está abaixo do peso!')
+} else if (condicao >= 18.5 && condicao < 25) {
+    console.log('Seu índice corporal IMC é de: ' + condicao.toFixed(0) + '.' + ' Você está no peso normal!')
+} else (condicao >= 25 && condicao < 30); {
+    console.log('Seu índice corporal IMC é de: ' + condicao.toFixed(0) + '.' + ' Você acima do peso!')
+} if (condicao >= 30 && condicao < 40) {
+    console.log('Seu índice corporal IMC é de: ' + condicao.toFixed(0) + '.' + ' Você tem obesidade! Vai caminhar!')  
+} else if (condicao >= 40) {
+    console.log('Seu índice corporal IMC é de: ' + condicao.toFixed(0) + '.' + ' Você está com obesidade grave. Vá ao pronto socorro IMEDIATAMENTE!')
+}
+
