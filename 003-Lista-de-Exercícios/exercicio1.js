50d8f6feb788bf7654066eac8c75ddbc65f5322d
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-21 14:50:58 -0300

    commit final?

--- 003-Lista-de-Exercícios/exercicio1.js
+++ 003-Lista-de-Exercícios/exercicio1.js
@@ -4,13 +4,13 @@ const nota3 = 5;
 
 const notaMedia = (nota1 + nota2 + nota3) / 3;
 
-console.log('Sua nota é ' + notaMedia);
+console.log('Sua nota é ' + notaMedia.toFixed(1));
 
 if (notaMedia < 5) {
     console.log('Você rodou, e terá que repetir o ano :(');
 } else if (notaMedia >= 5 && notaMedia < 7) {
     console.log('Você ficou em recuperação. Estude mais para passar de ano!');
-} else (notaMedia >= 7); {
-    console.log('Você passou de ano! Aproveite as férias!');
+} else if (notaMedia >= 7); {
+    console.log('Você passou de ano! Aproveite as férias :)');
 }
 
