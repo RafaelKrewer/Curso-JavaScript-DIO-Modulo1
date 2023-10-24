d78a46d05c4a5f0c11bed2a6e03c13c4dae256ba
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-24 17:25:15 -0300

    commit 2

--- 004-Funções/aula04.js
+++ 004-Funções/aula04.js
@@ -1,10 +1,2 @@
-function sayMyName(nome){
-    console.log('O meu nome é ' + nome);
-}
-function sayYourName(name){
-    console.log(' E o seu nome é ' + name);
-}
 
-sayMyName('Rafael');
-sayYourName('Bárbara');
 
