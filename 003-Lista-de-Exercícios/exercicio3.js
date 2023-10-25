80d499dd62386364ff5a9583ed99ba2265f01832
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-25 15:08:22 -0300

    switch finalizado

--- 003-Lista-de-Exercícios/exercicio3.js
+++ 003-Lista-de-Exercícios/exercicio3.js
@@ -1,7 +1,7 @@
 const preco = 100;
-const formaPagamento = 1;
+const formaPagamento = 2;
 
-if (formaPagamento === 1){
+/*if (formaPagamento === 1){
     console.log(preco - preco * 0.1);
 } else if(formaPagamento === 2){
     console.log(preco - preco * 0.15);
@@ -9,6 +9,21 @@ if (formaPagamento === 1){
     console.log(preco);
 } else if(formaPagamento === 4){
     console.log(preco + preco * 0.1);
+}*/
+
+switch(formaPagamento){
+    case 1:
+        console.log('Você escolheu pagar por débito! O preço total será: ' + preco - preco * 0.1);
+        break;
+    case 2:
+        console.log(preco - preco * 0.15);
+        break;
+    case 3:
+        console.log(preco);
+        break;
+    case 4:
+        console.log(preco + preco * 0.1);
+        break;
+        default:
+        break;
 }
-
-
