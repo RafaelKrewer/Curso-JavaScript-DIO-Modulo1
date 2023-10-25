64702ed942c42ed0fdacac310f65b25f8f73dab5
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-25 15:27:28 -0300

    switch final

--- 003-Lista-de-Exercícios/exercicio3.js
+++ 003-Lista-de-Exercícios/exercicio3.js
@@ -1,29 +1,22 @@
 const preco = 100;
-const formaPagamento = 2;
-
-/*if (formaPagamento === 1){
-    console.log(preco - preco * 0.1);
-} else if(formaPagamento === 2){
-    console.log(preco - preco * 0.15);
-} else if(formaPagamento === 3){
-    console.log(preco);
-} else if(formaPagamento === 4){
-    console.log(preco + preco * 0.1);
-}*/
+const formaPagamento = 7;
+const parcela = preco * 0.1;
+const total = parcela + preco;
 
 switch(formaPagamento){
     case 1:
-        console.log('Você escolheu pagar por débito! O preço total será: ' + preco - preco * 0.1);
+        console.log('Você escolheu pagar por débito! O preço total será: R$' + preco - preco * 0.1);
         break;
     case 2:
-        console.log(preco - preco * 0.15);
+        console.log('Você escolheu pagar por Pix! O preço total do produto será: R$' + preco - preco * 0.15);
         break;
     case 3:
-        console.log(preco);
+        console.log('Você escolheu pagar à vista! O preco total, sem desconto será de: R$' + preco);
         break;
     case 4:
-        console.log(preco + preco * 0.1);
+        console.log( 'Você escolheu pagar em mais de 2X! O preco total será de: R$' + total); 
         break;
-        default:
+    default:
+        console.log('Não aceitamos esse formato de pagamento!');
         break;
 }
