8b6884dc328a0611fe9206d12a9a97ae19584915
RafaelKrewer <cesar.krewer@gmail.com>
2023-10-30 16:04:18 -0300

    commit

--- 004-Funções/exercicio5.js
+++ 004-Funções/exercicio5.js
@@ -1,22 +1,26 @@
 const preco = 100;
 const pagamento = 4;
-const parcela = preco * 0.1;
-const total = parcela + preco;
-const pix = preco - (preco * 0.15);
-const debito = preco - (preco * 0.1);
+
+function desconto(valor, desconto){
+    return(valor - (valor * desconto / 100));
+}
+
+function juros(valor, juros){
+    return(valor + (valor * (juros / 100)));
+}
 
 switch(pagamento){
     case 1:
-        console.log('Você escolheu pagar por débito! O preço total do produto será: R$' + debito.toFixed(2));
+        console.log('Você escolheu pagar por débito! O preço total do produto será: R$' + desconto(preco, 10).toFixed(2));
         break;
     case 2:
-        console.log('Você escolheu pagar por Pix! O preço total do produto será: R$' + pix.toFixed(2));
+        console.log('Você escolheu pagar por Pix! O preço total do produto será: R$' + desconto(preco, 15).toFixed(2));
         break;
     case 3:
-        console.log('Você escolheu pagar em duas vezes! O preco total do produto, sem desconto será de: R$' + preco.toFixed(2));
+        console.log('Você escolheu pagar em duas vezes! O preco total do produto, sem desconto será: R$' + preco.toFixed(2));
         break;
     case 4:
-        console.log('Você escolheu pagar em mais de 2X! O preco total do produto será de: R$' + total.toFixed(2)); 
+        console.log('Você escolheu pagar em mais de 2X! O preco total do produto será de: R$' + desconto(preco, 10).toFixed(2)); 
         break;
     default:
         console.log('Não aceitamos esse tipo de cartão!');
