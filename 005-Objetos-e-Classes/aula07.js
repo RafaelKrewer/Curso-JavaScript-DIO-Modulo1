c836df0f1dbb82b791048c732817e0255aee93ff
RafaelKrewer <cesar.krewer@gmail.com>
2023-11-02 17:16:58 -0300

    commit

--- 005-Objetos-e-Classes/aula07.js
+++ 005-Objetos-e-Classes/aula07.js
@@ -16,6 +16,15 @@ class Pessoa {
 
 function compararPessoas(p1, p2){
     if(p1.idade > p2.idade){
-        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`)  
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`);  
+    } else if(p2.idade > p1.idade){
+        console.log(`${p2.nome} é mais velho(a) que ${p1.nome}`); 
+    } else{
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`);
     }
 }
+
+const rafael = new Pessoa('Rafael', 15, 2008);
+const cesar = new Pessoa('César', 45, 1978);
+
+compararPessoas(rafael, cesar);
