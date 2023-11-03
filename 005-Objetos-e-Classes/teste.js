44da997dd5584fcbd54759cc9d732fea8a5a8009
RafaelKrewer <cesar.krewer@gmail.com>
2023-11-03 14:57:33 -0300

    commit 59274099720524575645

--- 005-Objetos-e-Classes/teste.js
+++ 005-Objetos-e-Classes/teste.js
@@ -9,7 +9,7 @@ class Pessoa{
         this.altura = altura;
     }
 
-    descrever(rafael, cesar){
+    descrever(){
         return(`${this.nome}, seu índice IMC é de: ${imc}`);    
     }
 }
@@ -18,7 +18,7 @@ const rafael = new Pessoa('Rafael', 54, 1.67);
 const cesar = new Pessoa('César', 90, 1.83);
 
 
-    function calcularIMC(peso, altura){
+    function calcularIMC(){
     return rafael.peso / Math.pow(rafael.altura, 2);
 }
 
