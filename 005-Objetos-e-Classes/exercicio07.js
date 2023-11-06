1e6577500445d5e6d948d4b28298e642040126c0
RafaelKrewer <cesar.krewer@gmail.com>
2023-11-06 13:49:29 -0300

    fliberolu

--- 005-Objetos-e-Classes/exercicio07.js
+++ 005-Objetos-e-Classes/exercicio07.js
@@ -2,4 +2,17 @@ class Pessoa{
     nome;
     peso;
     altura;
+
+    constructor(nome, peso, altura){
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;   
+    }
+
+    IMC(){
+        return this.peso / (Math.pow(this.altura, 2));
+    }
 }
+
+Math.pow(this.altura, 2);
+const jose = new Pessoa('José', 70, 1.75);
