1928dd4fce386e356e5fb244721999729e606bbb
RafaelKrewer <cesar.krewer@gmail.com>
2023-11-07 16:50:38 -0300

    commit 63535

--- 006-Arrays-e-Estruturas-de-Repetição/aula08.js
+++ 006-Arrays-e-Estruturas-de-Repetição/aula08.js
@@ -1,2 +1,7 @@
-const alunos = ['João', 'Lucas', 'Guilherme', 'Maria'];
+const alunos = ['João', 'Lucas', 'Guilherme', 'Jordana'];
+
+alunos.push('Renan');
+
+alunos[4] = '';
+
 console.log(alunos[0]);
