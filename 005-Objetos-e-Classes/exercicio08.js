9657b2f39c264bce80767a1682786584c8fcf57a
RafaelKrewer <cesar.krewer@gmail.com>
2023-11-07 16:24:52 -0300

    commit

--- 005-Objetos-e-Classes/exercicio08.js
+++ 005-Objetos-e-Classes/exercicio08.js
@@ -15,16 +15,17 @@ class Pessoa{
 
     classIMC(){
         const imc = this.calcIMC();
+        
         if (imc < 18.5) {
-            console.log('Seu índice corporal IMC é de: ' + imc.toFixed(0) + '!' +' Você está abaixo do peso!')
-        }else if (imc >= 18.5 && imc < 25){
-            console.log('Seu índice corporal IMC é de: ' + imc.toFixed(0) + '!' +' Você está no peso normal!')
-        }else if (imc > 25 && imc < 30){
-            console.log('Seu índice corporal IMC é de: ' + imc.toFixed(0) + '!' +' Você está acima do peso!')
-        }else if (imc >= 30 && imc < 40){
-            console.log('Seu índice corporal IMC é de: ' + imc.toFixed(0) + '!' +'Você está com obesidade! Vai caminhar!')
-        }else if(imc >= 40){
-            console.log('Seu índice corporal IMC é de: ' + imc.toFixed(0) + '!' +' Você está com obesidade grave. Vá ao pronto socorro IMEDIATAMENTE!')
+            return('Seu índice corporal IMC é de: ' + imc.toFixed(0) + '!' +' Você está abaixo do peso!');
+        } else if (imc >= 18.5 && imc < 25){
+            return('Seu índice corporal IMC é de: ' + imc.toFixed(0) + '!' +' Você está no peso normal!');
+        } else if (imc > 25 && imc < 30){
+            return('Seu índice corporal IMC é de: ' + imc.toFixed(0) + '!' +' Você está acima do peso!');
+        } else if (imc >= 30 && imc < 40){
+            return('Seu índice corporal IMC é de: ' + imc.toFixed(0) + '!' +'Você está com obesidade! Vai caminhar!');
+        } else if(imc >= 40){
+            return('Seu índice corporal IMC é de: ' + imc.toFixed(0) + '!' +' Você está com obesidade grave. Vá ao pronto socorro IMEDIATAMENTE!');
         }
     }
 }
@@ -33,5 +34,5 @@ Math.pow(this.altura, 2);
 
 const jose = new Pessoa('José', 70, 1.75);
 const paulo = new Pessoa('Paulo', 80, 1.85);
-console.log(jose.calcIMC());
-console.log(paulo.calcIMC());
+console.log(jose.classIMC());
+console.log(paulo.classIMC());
