f37c665e0ddef53512fa9225097aa94d90a6a51c
RafaelKrewer <cesar.krewer@gmail.com>
2023-11-08 14:16:41 -0300

    commit inicial

--- Módulo 01/006-Arrays-e-Estruturas-de-Repetição/aula09.js
+++ Módulo 01/006-Arrays-e-Estruturas-de-Repetição/aula09.js
@@ -5,8 +5,7 @@ notas.push(7);
 notas.push(8);
 notas.push(2);
 notas.push(5);
-notas.push(8);
 
-const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4] + notas[5];
+const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
 
-console.log(soma / 6);
+console.log(soma / 5);
