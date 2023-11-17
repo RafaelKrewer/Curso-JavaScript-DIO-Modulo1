74162f6e5cfd8a0b4eb7fe2204b6b788de16694a
RafaelKrewer <cesar.krewer@gmail.com>
2023-11-17 14:00:03 -0300

    commit final

--- Módulo 01/003-Lista-de-Exercícios/exercicio01.js
+++ Módulo 01/003-Lista-de-Exercícios/exercicio01.js
@@ -1,27 +1,17 @@
-const nota1 = 4;
-const nota2 = 4;
+const nota1 = 5;
+const nota2 = 5;
 const nota3 = 5;
 
 const notaMedia = (nota1 + nota2 + nota3) / 3;
 const notaTotal = notaMedia;
 
-//console.log('Sua nota é ' + notaMedia.toFixed(1));
+if (notaMedia < 5) {
+    console.log('Reprovado');
+} else if (notaMedia >= 5 && notaMedia < 7) {
+    console.log('Recuperação');
+} else if (notaMedia >= 7) {
+    console.log('Aprovado');
+} else {
+    console.log('Número inválido');
+}
 
-/*if (notaMedia < 5) {
-    console.log('Você rodou, e terá que repetir o ano :(');
-}else if (notaMedia >= 5 && notaMedia < 7) {
-    console.log('Você ficou em recuperação. Estude mais para passar de ano!');
-}else if (notaMedia >= 7); {
-    console.log('Você passou de ano! Aproveite as férias :)');
-}*/
-
-switch(notaTotal){
-    case notaMedia < 5:
-        console.log('Você rodou, e terá que repetir o ano :(');
-        break;
-    case notaMedia >= 5 && notaMedia < 7:
-        console.log('Você ficou em recuperação. Estude mais para passar de ano!');
-        break;
-    case notaMedia >= 7:
-        console.log('Você passou de ano! Aproveite as férias :)');
-    }
